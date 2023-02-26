496e7c6ad21d4b2bb77739c8158f5e4513a17081
Shoubhik Maiti <shoubhikrajmaiti@gmail.com>
2023-02-26 13:09:10 +0000

    resolve conflicts

--- autode/bracket/imagepair.py
+++ autode/bracket/imagepair.py
@@ -13,6 +13,7 @@ from autode.opt.coordinates import CartesianCoordinates, OptCoordinates
 from autode.opt.optimisers.hessian_update import BofillUpdate
 from autode.opt.optimisers.base import _OptimiserHistory
 from autode.utils import work_in_tmp_dir, ProcessPool
+from autode.exceptions import CalculationException
 from autode.log import logger
 
 import autode.species.species
