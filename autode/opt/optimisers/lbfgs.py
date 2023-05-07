9571ed70676d2c962f16ffe1d50f7b2ebf3e7bf2
Shoubhik Maiti <shoubhikrajmaiti@gmail.com>
2023-05-07 21:12:43 +0100

    lbfgs update

--- autode/opt/optimisers/lbfgs.py
+++ autode/opt/optimisers/lbfgs.py
@@ -5,6 +5,7 @@ or storing Hessian would be difficult.
 """
 import numpy as np
 from typing import Optional
+from collections import deque
 from autode.opt.coordinates import CartesianCoordinates
 from autode.opt.optimisers.base import NDOptimiser
 from autode.log import logger
@@ -28,9 +29,9 @@ class LBFGSOptimiser(NDOptimiser):
 
         self.alpha = max_step
         self._max_vecs = abs(int(max_vecs))
-        self._s: Optional[np.ndarray] = None  # stores changes in coords -2D
-        self._y: Optional[np.ndarray] = None  # stores changes in grad - 2D
-        self._rho: Optional[np.ndarray] = None  # stores 1/(y.T @ s) - 1D
+        self._s = deque(maxlen=max_vecs)  # stores changes in coords -2D
+        self._y = deque(maxlen=max_vecs)  # stores changes in grad - 2D
+        self._rho = deque(maxlen=max_vecs)  # stores 1/(y.T @ s) - 1D
         self._h0: Optional[np.ndarray] = h0  # initial 1D diagonal of Hess
 
     def _initialise_run(self) -> None:
@@ -50,16 +51,6 @@ class LBFGSOptimiser(NDOptimiser):
             self._h0 = np.ones(shape=(self._max_vecs,))
         return None
 
-    @property
-    def _row_k(self) -> int:
-        """
-
-
-        Returns:
-            (int): The row number (or 1st dimension number for 1d arrays)
-        """
-        return (self.iteration + self._max_vecs) % self._max_vecs
-
     def _step(self) -> None:
 
         # NOTE: To reduce memory operations, self._s, self._y and self._rho are used
@@ -68,7 +59,6 @@ class LBFGSOptimiser(NDOptimiser):
         # from the list of stored vectors. row_k points to the current row
         # that should be written to.
         if self.iteration >= 1:
-            row_k = (self.iteration + self._max_vecs) % self._max_vecs
             # todo check above formula is it correct for python or fortran
             s = self._coords - self._history[-2]
             y = self._coords.g - self._history[-2].g
@@ -77,29 +67,42 @@ class LBFGSOptimiser(NDOptimiser):
                 logger.warning("Resetting y_s in LBFGS to 1.0")
                 y_s = 1.0
             # save in memory
-            self._s[row_k, :] = s
-            self._y[row_k, :] = y
-            self._rho[row_k] = 1.0 / y_s
+            self._s.append(s)
+            self._y.append(y)
+            self._rho.append(1.0 / y_s)
             # update the hessian diagonal
             y_norm = np.linalg.norm(y)
             if abs(y_norm) < 1.0e-16:
                 logger.warning("Resetting y_norm in LBFGS to 1.0")
             gamma = y_s / (y_norm**2)
             self._h0 *= gamma
+            step = _get_lbfgs_step_py(
+                self._coords.g,
+                self._s,
+                self._y,
+                self._rho,
+                self._h0,
+                self.iteration,
+            )
+        else:
+            g_size = np.linalg.norm(self._coords.g)
+            step = self._h0 * self._coords.g
+            step *= min(g_size, 1 / g_size)
 
         logger.info("Taking an L-BFGS step")
+        self._coords = self._coords + step
 
 
 # todo port this to cython after finishing checking
 def _get_lbfgs_step_py(
     grad: np.ndarray,
-    s_matrix: np.ndarray,
-    y_matrix: np.ndarray,
-    rho_array: np.ndarray,
+    s_matrix: deque,
+    y_matrix: deque,
+    rho_array: deque,
     hess_diag: np.ndarray,
     iteration: int,
 ):
-    max_vecs = s_matrix.shape[0]
+    max_vecs = len(s_matrix)
     q = grad.copy()
     a = np.zeros(max_vecs)
     # todo double check the formulas
@@ -109,22 +112,13 @@ def _get_lbfgs_step_py(
         iter_range = range(0, max_vecs)
 
     for i in reversed(iter_range):
-        row_k = (iteration + max_vecs) % max_vecs
-        mat_idx = i + row_k
-        if mat_idx >= max_vecs:
-            mat_idx -= max_vecs
-        # todo fix this!! i is not the same as iter_range
-        a[i] = rho_array[mat_idx] * np.dot(s_matrix[mat_idx], q)
-        q -= a[i] * y_matrix[mat_idx]
+        # todo fix this!! i is not the same as iter_range?
+        a[i] = rho_array[i] * np.dot(s_matrix[i], q)
+        q -= a[i] * y_matrix[i]
 
     q *= hess_diag  # use q as working space for z
     for i in iter_range:
-        row_k = (iteration + max_vecs) % max_vecs
-        mat_idx = i + row_k
-        if mat_idx >= max_vecs:
-            mat_idx -= max_vecs
-        beta = rho_array[mat_idx] * np.dot(y_matrix[mat_idx], q)
-        q += (a[i] - beta) * s_matrix[mat_idx]
+        beta = rho_array[i] * np.dot(y_matrix[i], q)
+        q += (a[i] - beta) * s_matrix[i]
 
     return -q
-
