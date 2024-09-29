5267b666c3bbf8eb0c15e1dc77311d03d534e7f2
Shoubhik Maiti <17470159+shoubhikraj@users.noreply.github.com>
2024-09-29 19:23:26 +0100

    refactor, add doc

--- autode/alignment.py
+++ autode/alignment.py
@@ -1,6 +1,7 @@
 """
 Align reactants and products to each other using graph isomorphism
 """
+from typing import TYPE_CHECKING, List
 from autode.transition_states.locate_tss import translate_rotate_reactant
 from autode.mol_graphs import reac_graph_to_prod_graph
 from autode.bond_rearrangement import BondRearrangement
@@ -8,39 +9,32 @@ from autode.geom import calc_rmsd
 from autode.species.complex import ReactantComplex
 from networkx import isomorphism
 
-
-def get_heavy_and_active_h_indices(
-    rct: ReactantComplex, prod, bond_rearr: BondRearrangement
-):
-    """Obtain indices of the heavy atoms, and any active H atoms (which
-    are included in the set for convenience of mapping)"""
-    assert rct.n_atoms == prod.n_atoms
-    rct_idxs = []
-    prod_idxs = []
-    for i in range(rct.n_atoms):
-        if rct.atoms[i].label != "H":
-            rct_idxs.append(i)
-        if prod.atoms[i].label != "H":
-            prod_idxs.append(i)
-
-    active_hs = []
-    for i in bond_rearr.active_atoms:
-        if rct.atoms[i].label == "H":
-            active_hs.append(i)
-
-    if len(active_hs) == 0:
-        return rct_idxs, prod_idxs
-    # TODO: remove Hs not attached to rxn centre
+if TYPE_CHECKING:
+    from autode.species import Species
 
 
-def get_rxn_core_indices(rct: ReactantComplex, bond_rearr):
-    """Obtain the core indices"""
+def get_rxn_core_indices(
+    rct: "Species", bond_rearr: "BondRearrangement"
+) -> List[int]:
+    """
+    Obtain the 'core' for a reactant and a set of bond rearrangements
+    which includes all non-hydrogen atoms, any H-atoms involved in the
+    reaction, and any H-atoms that have non-standard bonding pattern
+    (i.e. attached to more than one neighbour)
+
+    Args:
+        rct: The reactant (can be molecule or complex)
+        bond_rearr: Bond rearrangement for the reaction
+
+    Returns:
+        (list[int]): A list of indices of the core atoms
+    """
     assert rct.graph is not None
     idxs = set()
     for i in range(rct.n_atoms):
         if rct.atoms[i].label != "H":
             idxs.add(i)
-        elif rct.graph.degree[i] > 1:  # non-terminal H's
+        elif rct.atoms[i].label == "H" and rct.graph.degree[i] > 1:
             idxs.add(i)
         if i in bond_rearr.active_atoms:
             idxs.add(i)
@@ -48,23 +42,33 @@ def get_rxn_core_indices(rct: ReactantComplex, bond_rearr):
 
 
 def align_rct_prod(
-    rct: ReactantComplex, prod: ReactantComplex, bond_rearr: BondRearrangement
+    reactant: "Species", product: "Species", bond_rearr: "BondRearrangement"
 ):
-    """Align with graph"""
+    """
+    Align the reactant and product using their graphs, and the given
+    bond rearrangement. First aligns the 'core' atoms (excluding terminal
+    hydrogen atoms) and then the remaining H atoms. Modifies the reactant
+    and product objects in-place with the best mapping.
 
-    rct_rearr_graph = reac_graph_to_prod_graph(rct.graph, bond_rearr)
+    Args:
+        reactant: The reactant molecule/complex
+        product: The product molecule/complex
+        bond_rearr: Bond rearrangement for reaction
+    """
+
+    rct_rearr_graph = reac_graph_to_prod_graph(reactant.graph, bond_rearr)
     node_match = isomorphism.categorical_node_match("atom_label", "C")
     gm = isomorphism.GraphMatcher(
-        rct_rearr_graph, prod.graph, node_match=node_match
+        rct_rearr_graph, product.graph, node_match=node_match
     )
     # Initial mapping will ensure the correct connectivity
     init_map = next(gm.isomorphisms_iter())
-    prod.reorder_atoms(mapping={u: v for v, u in init_map.items()})
+    product.reorder_atoms(mapping={u: v for v, u in init_map.items()})
 
-    core_idxs = get_rxn_core_indices(rct, bond_rearr)
-    core_mapping = align_on_idxs(rct, prod, rct_rearr_graph, core_idxs)
+    core_idxs = get_rxn_core_indices(reactant, bond_rearr)
+    core_mapping = align_on_idxs(reactant, product, rct_rearr_graph, core_idxs)
 
-    return rct, prod
+    return None
 
 
 MAX_MAPS = 50
