07caa3cb4e2d99cf1fc2c52d6b2a62ed7f0f1123
shoub <shoubhikrajmaiti@gmail.com>
2025-07-19 20:15:43 +0530

    update

--- autode/species/alignment.py
+++ autode/species/alignment.py
@@ -1,3 +1,5 @@
+import numpy as np
+from scipy.optimize import minimize
 from autode.species import Complex
 from autode.bond_rearrangement import BondRearrangement
 
@@ -29,6 +31,58 @@ def forming_bonds_vdw_force_term(
     return e_sum
 
 
+def get_energy_rotate_translate(
+    x: np.ndarray,
+    cmplx: Complex,
+    bond_rearr: BondRearrangement,
+    return_coords: bool = False,
+):
+    """
+    Get the 'energy' based on hard sphere 1/r^4 repulsion, and
+    1/r^2 attraction on the forming bonds. The first species
+    in the complex is always considered stationary.
+
+
+    Args:
+        x:
+        cmplx:
+        bond_rearr:
+        return_coords:
+
+    Returns:
+
+    """
+    # Must have 6 DOF variables for each molecule
+    _x = x.ravel()
+    assert len(_x) == (cmplx.n_molecules - 1) * 6
+    assert cmplx.n_molecules > 1
+
+    edited_cmplx = cmplx.copy()
+    cmplx_coords = edited_cmplx.coordinates
+    for i in range(edited_cmplx.n_molecules):
+        # do not move first molecule
+        if i == 0:
+            continue
+        mol_idxs = edited_cmplx.atom_indexes(i)
+        mol_cog = cmplx_coords[mol_idxs].reshape(-1, 3).mean(axis=0)
+        theta_x, theta_y, theta_z = _x[i + 2 : i + 5]
+        edited_cmplx.rotate_mol([1, 0, 0], theta_x, i, mol_cog)
+        edited_cmplx.rotate_mol([0, 1, 0], theta_y, i, mol_cog)
+        edited_cmplx.rotate_mol([0, 0, 1], theta_z, i, mol_cog)
+        edited_cmplx.translate_mol(_x[i - 1 : i + 2], i)
+
+    total_en = 0.0
+    for i in range(edited_cmplx.n_molecules):
+        total_en += edited_cmplx.calc_repulsion(i)
+    total_en = total_en / edited_cmplx.n_molecules
+
+    bond_terms = forming_bonds_vdw_force_term(cmplx, bond_rearr)
+    if not return_coords:
+        return total_en + bond_terms
+    else:
+        return edited_cmplx.coordinates
+
+
 def create_aligned_complex_conformers(
     cmplx: Complex, bond_rearr: BondRearrangement
 ):
@@ -40,4 +94,27 @@ def create_aligned_complex_conformers(
         cmplx: The complex with more than one species
         bond_rearr: The bond rearrangement on the complex
     """
+    if cmplx.n_molecules < 2:
+        return None
+
     cmplx._generate_conformers()
+
+    n_dof = (cmplx.n_molecules - 1) * 6
+    for conf in cmplx.conformers:
+        print("Minimized one conformer")
+        cmplx.coordinates = conf.coordinates
+        x0 = np.zeros(n_dof)
+        res = minimize(
+            fun=get_energy_rotate_translate,
+            x0=x0,
+            method="l-bfgs-b",
+            args=(
+                cmplx,
+                bond_rearr,
+            ),
+        )
+        conf.coordinates = get_energy_rotate_translate(
+            res.x, cmplx, bond_rearr, return_coords=True
+        )
+
+    return None
