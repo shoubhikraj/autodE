0663ebda4e6f7cdc7d2230b9bf5225f078197f67
shoub <shoubhikrajmaiti@gmail.com>
2025-07-24 20:01:10 +0530

    final update

--- autode/species/alignment.py
+++ autode/species/alignment.py
@@ -1,8 +1,10 @@
+import itertools
 import math
 
 import numpy as np
 from scipy.optimize import minimize
 from autode.species import Complex
+from autode.conformers import Conformers, Conformer
 from autode.bond_rearrangement import BondRearrangement
 
 
@@ -101,6 +103,9 @@ def create_aligned_complex_conformers(
     if cmplx.n_molecules < 2:
         return None
 
+    if len(bond_rearr.fbonds) == 0:
+        raise RuntimeError("Something has gone wrong in bond rearrangement")
+
     cmplx._generate_conformers()
 
     n_dof = (cmplx.n_molecules - 1) * 6
@@ -122,3 +127,97 @@ def create_aligned_complex_conformers(
         )
 
     return None
+
+
+def prune_best_aligned_complex_conformers(
+    cmplx: Complex, bond_rearr: BondRearrangement
+):
+    """
+    Prune the complexes based on distance criteria
+
+    Args:
+        cmplx:
+        bond_rearr:
+
+    Returns:
+
+    """
+    fbonds = bond_rearr.fbonds
+
+    if len(fbonds) == 0 or cmplx.n_conformers == 0:
+        return None
+
+    all_rms_bond_ls = []
+    for i, conf in enumerate(cmplx.conformers):
+        rms_bond_l = math.sqrt(
+            sum(conf.distance(*fb) ** 2 for fb in fbonds) / len(fbonds)
+        )
+        all_rms_bond_ls.append(rms_bond_l)
+
+    min_rms_bond_l = min(all_rms_bond_ls)
+    new_conf_list = []
+    for i, conf in enumerate(cmplx.conformers):
+        if all_rms_bond_ls[i] < min_rms_bond_l * 1.3:
+            new_conf_list.append(conf)
+    print(f"Pruned to {len(new_conf_list)} conformers")
+    cmplx.conformers = Conformers(new_conf_list)
+    return None
+
+
+def get_best_pair_alignment(rct_cmplx: Complex, prod_cmplx: Complex):
+    """
+    Obtain a pair of conformers based on 3D geometrical similarity
+
+    Args:
+        rct_cmplx:
+        prod_cmplx:
+
+    Returns:
+
+    """
+    # TODO: simplify this part of code
+    if rct_cmplx.n_conformers == 0:
+        rct_cmplx.conformers = Conformers(
+            [Conformer(name=rct_cmplx.name, species=rct_cmplx)]
+        )
+    if prod_cmplx.n_conformers == 0:
+        prod_cmplx.conformers = Conformers(
+            [Conformer(name=prod_cmplx.name, species=prod_cmplx)]
+        )
+
+    def get_cme_mol(mol):
+        """Get Coulomb matrix eigenvalues"""
+        arr = np.zeros(shape=(mol.n_atoms, mol.n_atoms))
+        for i in range(mol.n_atoms):
+            for j in range(mol.n_atoms):
+                if i == j:
+                    arr[i, j] = 0.5 * mol.atoms[i].atomic_number ** 2.4
+                else:
+                    arr[i, j] = (
+                        mol.atoms[i].atomic_number * mol.atoms[j].atomic_number
+                    ) / mol.distance(i, j)
+        return np.linalg.eigvalsh(arr)
+
+    rct_cme_list = []
+    prod_cme_list = []
+    for conf in rct_cmplx.conformers:
+        rct_cme_list.append(get_cme_mol(conf))
+    for conf in prod_cmplx.conformers:
+        prod_cme_list.append(get_cme_mol(conf))
+
+    lowest_similarity = None
+    best_pair: tuple = tuple()
+    for rct_i, prod_j in itertools.product(
+        range(rct_cmplx.n_conformers), range(prod_cmplx.n_conformers)
+    ):
+        cme_1 = rct_cme_list[rct_i]
+        cme_2 = prod_cme_list[prod_j]
+        similarity = np.linalg.norm(cme_1 - cme_2)
+        if lowest_similarity is None or similarity < lowest_similarity:
+            lowest_similarity = similarity
+            best_pair = rct_i, prod_j
+
+    return (
+        rct_cmplx.conformers[best_pair[0]],
+        prod_cmplx.conformers[best_pair[1]],
+    )
