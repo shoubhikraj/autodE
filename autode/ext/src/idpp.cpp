2d1d1e33e1993d6846c4e755aa0c580cf85b85f9
Shoubhik Maiti <17470159+shoubhikraj@users.noreply.github.com>
2026-01-26 16:41:27 +0000

    bugfix update 2

--- autode/ext/src/idpp.cpp
+++ autode/ext/src/idpp.cpp
@@ -255,7 +255,7 @@ namespace autode {
                 auto grad_prefac = (-2.0) / dist_pow_4
                         + 6.0 * (*target_d_ptr) / dist_pow_5
                         - 4.0 * std::pow(*target_d_ptr, 2) / dist_pow_6;
-                grad_prefac *= (*d_wt_ptr)
+                grad_prefac *= (*d_wt_ptr);
 
                 // gradient terms
                 dist_vec *= grad_prefac;
